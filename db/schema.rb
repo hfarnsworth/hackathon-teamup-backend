6a1f87b98ee9a771104115f9b9acdba2b55e8cef
hfarnsworth <havilahfarnsworth@gmail.com>
2020-08-12 16:48:09 -0500

    changed database from SQLite to Postgre for Heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_13_092101) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "collaborators", force: :cascade do |t|
     t.string "name"
     t.string "role"
